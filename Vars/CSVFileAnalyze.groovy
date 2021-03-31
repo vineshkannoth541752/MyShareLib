8762cba335f388b09c586ae1b5744e913458657b
vineshkannoth541752 <80946269+vineshkannoth541752@users.noreply.github.com>
2021-03-31 06:01:04 +0530

    Create CSVFileAnalyze.groovy
    This groovy script analyzes the input CSV file and produces the release status report file

---
+++ Vars/CSVFileAnalyze.groovy
@@ -0,0 +1,149 @@
+import org.apache.commons.csv.*
+
+import java.util.stream.*
+
+@NonCPS
+
+void call()
+
+{
+
+
+String[] HEADERS = ["AppID","AppName","Environment","ReleaseVersion","Status"]
+
+String outputHeader="App ID,App Name,Release Version,Environments Passed,Environment Failed,Comments";
+
+
+Reader filereader = new FileReader("$WORKSPACE\\InputFIleCaseStudy.csv");
+
+Iterable<CSVRecord> records = CSVFormat.DEFAULT
+
+		.withHeader(HEADERS)
+
+		.withFirstRecordAsHeader()
+
+		.parse(filereader);
+
+
+
+StringBuilder output = new StringBuilder();
+
+output.append(outputHeader);
+
+
+
+Map<String, List<CSVRecord>> recordFiltered =   StreamSupport
+
+		.stream(records.spliterator(), false).
+
+		collect(Collectors.groupingBy({record -> record.get("AppID")+"-"+record.get("AppName")+"-"+record.get("ReleaseVersion")} ));
+
+		
+
+for (Map.Entry<String, List<CSVRecord>> entry : recordFiltered.entrySet()) {
+
+
+	List<CSVRecord> buildList = entry.getValue();	
+
+	List<CSVRecord> failedList = buildList.stream().filter({f -> f.get("Status").contains("Failed")})
+
+	.collect(Collectors.toList());
+
+	
+
+	List<CSVRecord> passedList = buildList.stream().filter({f -> !f.get("Status").contains("Passed")})
+
+	.collect(Collectors.toList());
+
+	
+
+	String delimitter = ",";
+
+	String passedEnvList = "NA";
+
+	String failedEnvList = "NA";
+
+	String comments = "NA";
+
+	
+
+	if (passedList != null && passedList.size() > 0) {
+
+		passedEnvList = passedList.stream().map({mp -> mp.get("Environment")}).collect(Collectors.joining("|"));
+
+		List<String> passComments = passedList.stream().map({mp -> String.format(mp.get("Status"))}).collect(Collectors.toList());
+		
+		if(!passComments.isEmpty()){
+		comments = passComments.get(0);
+		}
+
+	}
+
+	if (failedList != null && failedList.size() > 0) {
+
+		failedEnvList = failedList.stream().map({mp -> mp.get("Environment")}).collect(Collectors.joining("|"));
+
+		List<String> failComments = failedList.stream().map({mp -> String.format(mp.get("Status"))}).collect(Collectors.toList());
+		if(!failComments.isEmpty()){
+		comments = failComments.get(0);
+		}
+;
+	}
+
+	
+
+	output.append('\n');
+
+	output.append(buildList.get(0).get("AppID") + delimitter);
+
+	output.append(buildList.get(0).get("AppName") + delimitter);
+
+	output.append(buildList.get(0).get("ReleaseVersion") + delimitter);
+
+	output.append(passedEnvList + delimitter);
+
+	output.append(failedEnvList + delimitter);
+
+	output.append(comments);
+
+
+
+}
+
+
+
+PrintWriter writer;
+
+try {
+
+	writer = new PrintWriter(new File("$WORKSPACE\\Release_Status.csv"))
+
+	writer.write(output.toString());
+
+	System.out.println("done!");
+
+	println("____________________________________________")
+
+	println ("${output}")
+
+	println("____________________________________________")
+
+}
+
+
+
+catch (FileNotFoundException e) {
+
+	System.out.println(e.getMessage());
+
+}
+
+finally {
+
+	writer.close();
+
+}
+
+
+
+}
