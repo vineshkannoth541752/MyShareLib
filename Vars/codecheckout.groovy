7fb71987f4ebfb58d8b5fa4cba58855d15dc3393
vineshkannoth541752 <80946269+vineshkannoth541752@users.noreply.github.com>
2021-03-31 12:27:31 +0530

    Update codecheckout.groovy

--- Vars/codecheckout.groovy
+++ Vars/codecheckout.groovy
@@ -1,6 +1,6 @@
 void call()
 { 
           git branch: 'main',
-               // credentialsId: '  ',
+                credentialsId: '456bc367-2510-4b13-a2fe-95db58a4dbe8',
                 url: 'https://github.com/vineshkannoth541752/JenkinPipeline.git'
 }
